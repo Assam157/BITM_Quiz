dc438d3c6e9bd68706e4a6ebf6d08b9f2b84216d
Assam157 <asasm8106@gmail.com>
2025-12-29 23:09:46 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PostQuestionForm from "./components/QuestionSubmittingForm"
+
+function App() {
+  return (
+    <div className="App">
+      <PostQuestionForm/>
+    </div>
+  );
+}
+
+export default App;
